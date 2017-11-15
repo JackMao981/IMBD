0cf2f5a9d4ff54680523dfe318cc6c7834e39851
khincce <ksegenchuk@wpi.edu>
2017-11-15 18:44:28 -0500

    I think Jonathan's code works now, but the list files are being wonky for me

--- src/GraphSearchEngineImpl.java
+++ src/GraphSearchEngineImpl.java
@@ -4,28 +4,7 @@ public class GraphSearchEngineImpl implements GraphSearchEngine {
 
     private List<Node> mVisited;
     private Queue<Node> mToVisit;
-    private List<Node> mShortestPath;
-    private List<NodeDistance> mNodeDistances;
-    private int radius;
-
-    private class NodeDistance {
-        private int mDistance;
-        private Node mNode;
-
-        private NodeDistance(int distance, Node node) {
-            mDistance = distance;
-            mNode = node;
-        }
-
-        private int getDistance() {
-            return this.mDistance;
-        }
-
-        private Node getNodeWithDistance() {
-            return this.mNode;
-        }
-    }
-
+    private Map<Node, Integer> mNodeDistances;
 
     /**
      * Creates a GraphSearcher which implements GraphSearchEngine, and is capable of
@@ -34,10 +13,7 @@ public class GraphSearchEngineImpl implements GraphSearchEngine {
     public GraphSearchEngineImpl() {
         mVisited = new ArrayList<Node>();
         mToVisit = new LinkedList<Node>();
-        mNodeDistances = new ArrayList<NodeDistance>();
-        mShortestPath = new ArrayList<Node>();
-        radius = 0;
-
+        mNodeDistances = new HashMap<Node, Integer>();
     }
 
     /**
@@ -48,44 +24,44 @@ public class GraphSearchEngineImpl implements GraphSearchEngine {
      * @return the path from s to t, including s and t
      */
     public List<Node> findShortestPath(Node s, Node t) {
+        int radius = 0;
+        List<Node> shortestPath = new ArrayList<Node>();
+
         mToVisit.add(s);
-        /**
-         * Searches through the graph until node t is found, building a list of visited nodes
-         * and their distances from s.
-         */
+
         while (mToVisit.size() > 0) {
             Node visitedNode = mToVisit.remove();
             mVisited.add(visitedNode);
-            mNodeDistances.add(new NodeDistance(radius, visitedNode));
+            mNodeDistances.put(visitedNode, radius);
             if (visitedNode.equals(t))
                 break;
 
-            final Collection<? extends Node> neighbors = mVisitedNode.getNeighbors();
+            final Collection<? extends Node> neighbors = visitedNode.getNeighbors();
             for (Iterator<? extends Node> i = neighbors.iterator(); i.hasNext(); ) {
                 Node neighbor = i.next();
                 if (!mVisited.contains(neighbor) && !mToVisit.contains(neighbor)){
-                    mToVisit.add(s);
+                    mToVisit.add(neighbor);
                 }
 
             }
             radius++;
         }
-        mShortestPath.add(t);
-        /**
-         * Backtracks from t through the list of visited nodes and their distances, until
-         * a shortest path to node s is generated. After the path to s is generated, s is added
-         * to the shortest path, completing the shortest path.
-         */
+        shortestPath.add(t);
 
-        for (int i = radius - 1; i > 0; radius--) {
-            for (int j = 0; j < mNodeDistances.size(); j++) {
-                if (mNodeDistances.get(j).getDistance() == i) {
-                    mShortestPath.add(mNodeDistances.get(j).getNodeWithDistance());
-                    break;
+        Node currentNode = t;
+        for ( ; radius > 0; radius--) {
+            Set<Node> possibleNodes = mNodeDistances.keySet();
+            for (Iterator<? extends Node> i = possibleNodes.iterator(); i.hasNext(); ) {
+                Node node = i.next();
+
+                if (mNodeDistances.get(node) == radius && node.getNeighbors().contains(currentNode)) {
+                    shortestPath.add(node);
+                    currentNode = node;
                 }
             }
         }
-        mShortestPath.add(s);
-        return mShortestPath;
+        shortestPath.add(s);
+
+        return shortestPath;
     }
 }
