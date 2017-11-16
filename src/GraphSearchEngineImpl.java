75481fcff5dad23915d2b914726498f17b7439d4
khincce <ksegenchuk@wpi.edu>
2017-11-15 19:56:29 -0500

    might be fixed

--- src/GraphSearchEngineImpl.java
+++ src/GraphSearchEngineImpl.java
@@ -6,8 +6,8 @@ public class GraphSearchEngineImpl implements GraphSearchEngine {
     private Map<Node, Integer> mNodeDistances;
 
     /**
-     * Creates a GraphSearcher which implements GraphSearchEngine, and is capable of
-     * finding and returning the shortest path between two nodes.
+     * Creates a GraphSearcher which is capable of finding and returning a List of Nodes
+     * representing the shortest path between two nodes
      */
     public GraphSearchEngineImpl() {
         mToVisit = new LinkedList<Node>();
@@ -16,22 +16,21 @@ public class GraphSearchEngineImpl implements GraphSearchEngine {
 
     /**
      * Finds and returns the shortest path between two nodes
-     *
      * @param s the start node.
      * @param t the target node.
      * @return the path from s to t, including s and t
      */
     public List<Node> findShortestPath(Node s, Node t) {
         int radius = 0;
-        List<Node> shortestPath = new ArrayList<Node>();
 
         mToVisit.add(s);
 
         while (mToVisit.size() > 0) {
             Node visitedNode = mToVisit.remove();
+            if (visitedNode.equals(t)) {
+                return backtrack(s, t, radius);
+            }
             mNodeDistances.put(visitedNode, radius);
-            if (visitedNode.equals(t))
-                break;
 
             final Collection<? extends Node> neighbors = visitedNode.getNeighbors();
             for (Iterator<? extends Node> i = neighbors.iterator(); i.hasNext(); ) {
@@ -42,15 +41,28 @@ public class GraphSearchEngineImpl implements GraphSearchEngine {
             }
             radius++;
         }
+
+        return null;
+    }
+
+    /**
+     * Backtracks from t to s to find the List that constitutes the shortest path
+     * @param s the start node
+     * @param t the end node
+     * @param lengthOfShortestPath the distance from s to t
+     * @return
+     */
+    private List<Node> backtrack(Node s, Node t, int lengthOfShortestPath) {
+        List<Node> shortestPath = new ArrayList<Node>();
         shortestPath.add(t);
 
         Node currentNode = t;
-        for ( ; radius > 0; radius--) {
+        for (int i = lengthOfShortestPath - 1 ; i > 0; i--) {
             Set<Node> possibleNodes = mNodeDistances.keySet();
-            for (Iterator<? extends Node> i = possibleNodes.iterator(); i.hasNext(); ) {
-                Node node = i.next();
+            for (Iterator<? extends Node> j = possibleNodes.iterator(); j.hasNext(); ) {
+                Node node = j.next();
 
-                if (mNodeDistances.get(node) == radius && node.getNeighbors().contains(currentNode)) {
+                if (mNodeDistances.get(node) == lengthOfShortestPath && node.getNeighbors().contains(currentNode)) {
                     shortestPath.add(node);
                     currentNode = node;
                 }
